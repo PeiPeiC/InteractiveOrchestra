27aacbebdbee8c035698499e4092fb4cb10c88d7
Bo Zhou <2571442z@student.gla.ac.uk>
2023-10-28 13:08:00 +0100

    Basic design

--- opencv_project/webcam_app/views.py
+++ opencv_project/webcam_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request,'webcam_app/home.html')
+
+def violin(request):
+    return render(request,'webcam_app/violin.html')
